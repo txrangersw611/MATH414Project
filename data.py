d474dd111525326e7a709f9e256384618b20e7dd
Scott Walker <walker.scotty@gmail.com>
2023-04-14 14:47:17 +0000

    framework

--- data.py
+++ data.py
@@ -1,1 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+
+def load_data(x_path): 
+    return pd.read_csv(x_path)
+
+
+def split_data(data):
+    y = data.loc[-1,:] #just putting some garbo here (need to get the labels)
+    x = data.drop[-1] #getting features
+        
+    return train_test_split(x, y, test_size=.8)
+
+
+def preprocess_x(data):
+    #something with wavelets
+    return data
 
--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+import itertools
+
+import pandas as pd
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+
+from data import load_data, preprocess_x, split_data
+
+
+def main():
+    #load the Data
+    data = load_data("data.csv")
+
+    #split into train and test data
+    train_x, train_y, test_x, test_y = split_data(data)
+
+    #might need this to anylize the data with wavelets
+    processed_x_train = preprocess_x(train_x)
+    processed_x_test = preprocess_x(test_x)
+
+
+    #train and test the model
+    model = tree.DecisionTreeClassifier() #need to decide what model were gonna do (how are we gonna include wavelts)
+    model.fit(processed_x_train, train_y)
+
+
+    #get accuracy from model
+
+
+    #print results
+
+
+
+if __name__ == "__main__":
+    main()
 
