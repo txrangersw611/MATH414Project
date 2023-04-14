50392831958bc0f5364c7557850434f00efb7fca
Scott Walker <walker.scotty@gmail.com>
2023-04-14 16:15:40 +0000

    updated some stuff

--- data.py
+++ data.py
@@ -1,23 +1,27 @@
 import pandas as pd
 import numpy as np
 import pywt 
+import scipy.io.wavfile as wavfile
 from sklearn.model_selection import train_test_split
 
 def load_data(x_path): 
-    return pd.read_csv(x_path)
+    return wavfile.read(x_path)
 
 
 def split_data(data):
-    y = data.loc[-1,:] #just putting some garbo here (need to get the labels)
+    y = np.empty()
     x = data.drop[-1] #getting features
-        
+
     return train_test_split(x, y, test_size=.8)
 
 
 def waveletTransform(data):
     #creating Daubechies wavelet objext
-    wavelet = pywt.Wavelet('db1')
+    wavelet = pywt.Wavelet('db4')
 
+    #extract coefficients from this wavelet
+    coeffs = pywt.wavedec(data, wavelet, level=6) #doing 6 because thats what the research paper did
     
-    return data
+    features = np.sum(coeffs**2)
+    return features
 
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import itertools
-
 import pandas as pd
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn import tree
 
 from data import load_data, waveletTransform, split_data
 
 
-def main():
+def model(file):
     #load the Data
-    data = load_data("data.csv")
+    sample_rate, data = load_data("data.csv")
 
     #split into train and test data
     train_x, train_y, test_x, test_y = split_data(data)
@@ -24,14 +23,19 @@ def main():
     model = tree.DecisionTreeClassifier() #need to decide what model were gonna do (how are we gonna include wavelts)
     model.fit(processed_x_train, train_y)
 
-
     #get accuracy from model
-
+    preds = model.predict(processed_x_test)
+    score = accuracy_score(test_y, preds)
 
     #print results
+    print("The accuracy of the model is", score)
+
+    #######################################################
+    #testing for the inputted file
 
 
 
 if __name__ == "__main__":
-    main()
+    file = input("Enter bird call file name")
+    model(file)
 
