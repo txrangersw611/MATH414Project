28849c3cc19645cd9f5662a096a3d5fb507e4428
Scott Walker <walker.scotty@gmail.com>
2023-04-19 14:20:26 +0000

    working on load_data

--- data.py
+++ data.py
@@ -3,16 +3,42 @@ import numpy as np
 import pywt 
 import scipy.io.wavfile as wavfile
 from sklearn.model_selection import train_test_split
+import os
 
 def load_data(x_path): 
-    return wavfile.read(x_path)
+    #gets all the files in the directory
+    files = os.listdir(x_path)
+
+    #data is 3D array, each index is the wav files transformed into a 2D array of amplitudes (I think)
+    data = np.empty(0) 
+
+    # name = os.path.join(x_path, "ZM08.wav")
+    # rate, data = wavfile.read(name)
+    # print(data.shape)
+
+    #running read function on every file in directory
+    for file in files:
+        name = os.path.join(x_path, file)
+        rate, amplitudes = wavfile.read(name)
+        print(amplitudes)
+        data = np.append(data, amplitudes)
+        print(data)
+        print("-------------------")
+
+
+    return data
 
 
 def split_data(data):
-    y = np.empty()
-    x = data.drop[-1] #getting features
+    y = np.empty(100)
 
-    return train_test_split(x, y, test_size=.8)
+    #making y filled with 10 1'2, 10 2's, etc
+    for i in range(100):
+        y[i] = i/10
+        print(i/10)
+
+    print(y)
+    return train_test_split(data, y, test_size=.8, shuffle=True)
 
 
 def waveletTransform(data):
