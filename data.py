80780daed77a743d7b5cfee46cee2dafed6c1dbe
sydney-beeler <sydney.beeler@gmail.com>
2023-04-20 21:53:20 -0500

    Fixed typos, made more adaptable, added k-fold cross validation

--- data.py
+++ data.py
@@ -3,57 +3,79 @@ import numpy as np
 import pywt 
 import scipy.io.wavfile as wavfile
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 import os
+
 
 def load_data(x_path): 
     #gets all the files in the directory
     files = os.listdir(x_path)
 
     #data is array of energy in each wave
-    data = np.empty(58)
+    data = np.empty(len(files))
+    labels = [None] * len(files)
 
     #running read function on every file in directory
     for i, file in enumerate(files):
         name = os.path.join(x_path, file)
         rate, amplitudes = wavfile.read(name)
-        data[i] = waveletTransform(amplitudes)
+        data[i] = wavelet_transform(amplitudes)
+        labels[i] = file[0:2]
 
-    return data
+    return data, labels
 
 
-def split_data(data):
+def split_data(data, labels):
     """
-    CC is NORTHERN CARDINAL
-    CR is BLUE JAY
-    DP is DOWNY WOODPECKER
-    ST is AMERICAN GOLDFINCH
-    ZM is MOURNING DOVE
+    Splits set into 80% training data and 20% testing data. Returns x_train, x_test, y_train, y_test.
     """
-    y = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-         2,2,2,2,2,2,2,2,2,2,2,2,
-         3,3,3,3,3,3,3,3,3,3,
-         4,4,4,4,4,4,4,4,4,4,
-         5,5,5,5,5,5,5,5,5,5]
-    y = np.array(y)
 
     #Code for testing 
-    avg_printer(y, data)
+    # avg_printer(labels, data)
 
-    return train_test_split(data, y, train_size=.8, shuffle=True)
+    return train_test_split(data, labels, train_size=0.8, shuffle=True)
 
 
-def waveletTransform(data):
-    #creating Daubechies wavelet objext
+def train_test_folds(num_folds: int, shuffle: True, features, labels):
+    skf = StratifiedKFold(n_splits=num_folds, shuffle=shuffle)
+    scores = np.empty(num_folds)
+
+    # loop through each fold
+    for i, (train_index, test_index) in enumerate(skf.split(features, labels)):
+        # determine training and test sets for fold
+        x_train = np.array(features)[train_index]
+        y_train = np.array(labels)[train_index]
+        x_test = np.array(features)[test_index]
+        y_test = np.array(labels)[test_index]
+
+        # train model
+        model = tree.DecisionTreeClassifier().fit(x_train.reshape(-1,1), y_train)
+
+        # get predictions
+        preds = model.predict(x_test.reshape(-1,1))
+        
+        # get accuracy score
+        score = accuracy_score(y_test, preds)
+        scores[i] = score
+
+    return scores
+
+
+def wavelet_transform(data):
+    #creating Daubechies wavelet object
     wavelet = pywt.Wavelet('db4')
 
     #extract coefficients from this wavelet
-    coeffs = pywt.wavedec(data, wavelet, level=6) #doing 6 because thats what the research paper did
+    coeffs = pywt.wavedec(data, wavelet) 
     
     energy = 0
     for coeff in coeffs:
         energy += np.sum(coeff ** 2)
 
     return energy
+
 
 def avg_printer(y, data):
     print("-------------------")
--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 import pandas as pd
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from sklearn import tree
-
-from data import load_data, waveletTransform, split_data
+from data import load_data, wavelet_transform, split_data, train_test_folds
 
 
 def model():
-    #load the Data (data is a array of all the energies of each wave)
-    data = load_data("data")
-
+    #load the Data (data is an array of all the energies of each wave)
+    data, labels = load_data("data")
 
     #split into train and test data
-    train_x, test_x, train_y, test_y = split_data(data)
+    train_x, test_x, train_y, test_y = split_data(data, labels)
 
+    # #train and test the model
+    # model = tree.DecisionTreeClassifier() #need to decide what model to use & how to incorporate wavelets
+    # model.fit(train_x.reshape(-1,1), train_y)
 
-    #train and test the model
-    model = tree.DecisionTreeClassifier() #need to decide what model were gonna do (how are we gonna include wavelts)
-    model.fit(train_x.reshape(-1,1), train_y)
+    # #get accuracy from model
+    # preds = model.predict(test_x.reshape(-1,1))
+    # score = accuracy_score(test_y, preds)
 
-
-    #get accuracy from model
-    preds = model.predict(test_x.reshape(-1,1))
-    score = accuracy_score(test_y, preds)
-
+    scores = train_test_folds(5, True, data, labels)
+    print(scores)
+    print(max(scores))
 
     #print results
-    print("The accuracy of the model is", score)
-
+    # print("The accuracy of the model is", score)
 
     #######################################################
     #testing for the inputted file
